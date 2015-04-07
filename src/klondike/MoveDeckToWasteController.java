6a232145185f1093dcdbc90abbb7c3191b52fd20
Luis Quishpi <luisquishpi @138.4.205.235>
2015-04-07 20:41:19 +0200

    Se crea método move() en MoveDeckToWasterController update MoveDeckToWasterControllerTest

--- src/klondike/MoveDeckToWasteController.java
+++ src/klondike/MoveDeckToWasteController.java
@@ -2,8 +2,26 @@ package klondike;
 
 public class MoveDeckToWasteController {
 
+    private Board board;
+    public MoveDeckToWasteController(){
+        
+    }
+    public MoveDeckToWasteController(Board board) {
+        this.board=board;
+    }
+
     public boolean isMovedDeckToWaste(Card card) {
         return true;
     }
+    public Board getBoard() {
+        return board;
+    }
+    public void setBoard(Board board) {
+        this.board = board;
+    }
+    public void move() {
+        
+    }
+   
 
 }
--- src/test/MoveDeckToWasteControllerTest.java
+++ src/test/MoveDeckToWasteControllerTest.java
@@ -1,6 +1,10 @@
 package test;
 
 import static org.junit.Assert.*;
+
+import java.util.List;
+
+import klondike.Board;
 import klondike.Card;
 import klondike.MoveDeckToWasteController;
 
@@ -10,16 +14,25 @@ import org.junit.Test;
 public class MoveDeckToWasteControllerTest {
 
     private MoveDeckToWasteController moveDeckToWasteController;
+    Board board;
     
     @Before
     public void before() {
-        moveDeckToWasteController = new MoveDeckToWasteController();
+        board =new Board();
+        List<Card> cards = null;
+        board.setDeck(cards);
+        moveDeckToWasteController = new MoveDeckToWasteController(board);
     }
 
     @Test
     public void isMovedDeckToWasteTest() {
         Card card = new Card();
         assertTrue(moveDeckToWasteController.isMovedDeckToWaste(card));
+    }
+    @Test
+    public void moveFullDeckToEmptyWasteTest(){
+        moveDeckToWasteController.move();
+        assertEquals(3, board.getWaste().size());
     }
 
 }
