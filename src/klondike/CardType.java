b31c4bec9d20a5db0fa1cb9ff0cff4f385139518
Luis Quishpi <luisquishpi @138.4.205.235>
2015-04-10 17:10:39 +0200

    Nunca se usó los valores de Type, se liminan

--- src/klondike/CardType.java
+++ src/klondike/CardType.java
@@ -1,14 +1,5 @@
 package klondike;
 
 public enum CardType {
-    CORAZON(0), PICA(1), DIAMENTE(2), TREBOL(3);
-    private int puestoCardType;
-
-    public int getPuestoCardType() {
-        return puestoCardType;
-    }
-
-    private CardType(int puestoCardType) {
-        this.puestoCardType = puestoCardType;
-    }
+    CORAZON, PICA, DIAMENTE, TREBOL;
 }
