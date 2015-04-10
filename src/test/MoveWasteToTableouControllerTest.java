53c42456d8de82f1fc6b839183253b5c58b78939
Luis Quishpi <luisquishpi @138.4.205.235>
2015-04-10 17:07:51 +0200

    INIT Refactoring

--- src/test/MoveWasteToTableouControllerTest.java
+++ src/test/MoveWasteToTableouControllerTest.java
@@ -15,41 +15,48 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class MoveWasteToTableouControllerTest {
-
     MoveWasteToTableouController moveWasteToTableouController;
+
     Board board;
+
     Card lastWasteCard;
-    int indexPalo=0;
+
+    int indexPalo = 0;
+
     List<Card> listCard;
+
     @Before
     public void before() {
         board = new Board();
-        board.getWaste().add(new Card(3,true,CardType.CORAZON));
-        board.getWaste().add(new Card(8,true,CardType.PICA));
-        board.getWaste().add(new Card(9,true,CardType.CORAZON));
-        listCard=new ArrayList<Card>();
-        listCard.add(new Card(10,true,CardType.TREBOL));
+        board.getWaste().add(new Card(3, true, CardType.CORAZON));
+        board.getWaste().add(new Card(8, true, CardType.PICA));
+        board.getWaste().add(new Card(9, true, CardType.CORAZON));
+        listCard = new ArrayList<Card>();
+        listCard.add(new Card(10, true, CardType.TREBOL));
         board.getTableous().add(new Tableou(new ArrayList<Card>(listCard)));
-        lastWasteCard=new Card();
-        lastWasteCard = board.getWaste().get(board.getWaste().size()-1);
-        
+        lastWasteCard = new Card();
+        lastWasteCard = board.getWaste().get(board.getWaste().size() - 1);
         moveWasteToTableouController = new MoveWasteToTableouController(board);
     }
 
     @Test
-    public void isLastTableouCardHighTest(){
-        assertTrue(moveWasteToTableouController.isLastTableouCardHighTest(indexPalo,lastWasteCard));
+    public void isLastTableouCardHighTest() {
+        assertTrue(moveWasteToTableouController.isLastTableouCardHighTest(indexPalo, lastWasteCard));
     }
+
     @Test
-    public void isLastTableouCardVisibleTest(){
+    public void isLastTableouCardVisibleTest() {
         assertTrue(moveWasteToTableouController.isLastTableouCardVisible(indexPalo));
     }
+
     @Test
-    public void isLastTableouCardOtherColorTest(){
-        assertTrue(moveWasteToTableouController.isLastTableouCardOtherColor(indexPalo,lastWasteCard));
+    public void isLastTableouCardOtherColorTest() {
+        assertTrue(moveWasteToTableouController.isLastTableouCardOtherColor(indexPalo,
+                lastWasteCard));
     }
+
     @Test
-    public void isMovedWasteCardToTableouTest(){
+    public void isMovedWasteCardToTableouTest() {
         moveWasteToTableouController.move(indexPalo);
         assertEquals(2, board.getTableous().get(indexPalo).getListCard().size());
         assertEquals(2, board.getWaste().size());
