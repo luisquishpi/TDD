bae7c755a6ac77875c7047eec18dc6aa15b66321
Luis Quishpi <luisquishpi @138.4.205.235>
2015-04-10 04:32:38 +0200

    Init MoveWasteToFoundationControllerTest

---
+++ src/test/MoveWasteToFoundationControllerTest.java
@@ -0,0 +1,14 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MoveWasteToFoundationControllerTest {
+
+    @Test
+    public void test() {
+        fail("Not yet implemented");
+    }
+
+}
