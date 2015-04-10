2316d58f7c0dbb8bee6464a926f508519e3a8d91
Luis Quishpi <luisquishpi @138.4.205.235>
2015-04-10 17:41:00 +0200

    Refactoring  move() y format al código

--- src/klondike/MoveDeckToWasteController.java
+++ src/klondike/MoveDeckToWasteController.java
@@ -5,49 +5,49 @@ import java.util.List;
 public class MoveDeckToWasteController {
 
     private Board board;
-    List<Card> waste;
-    List<Card> deck;
-    public MoveDeckToWasteController(){
-        
-    }
-    public MoveDeckToWasteController(Board board) {
-        this.board=board;
-        waste=board.getWaste();
-        deck=board.getDeck();
-    }
 
-    public boolean isMovedDeckToWaste(Card card) {
-        return true;
-    }
-    public Board getBoard() {
-        return board;
-    }
-    public void setBoard(Board board) {
-        this.board = board;
+    List<Card> waste;
+
+    List<Card> deck;
+
+    public MoveDeckToWasteController(Board board) {
+        setBoard(board);
+        waste = board.getWaste();
+        deck = board.getDeck();
     }
     public void move() {
-        if(waste.isEmpty()){
-            for(int i=1;i<=3;i++){
-                move(deck.get(deck.size()-i));
+        if (waste.isEmpty()) {
+            for (int i = 1; i <= 3; i++) {
+                move(deck.get(deck.size() - i));
             }
-        }
-        else{
-            Card card=new Card();
-            card=board.getDeck().get(board.getDeck().size()-1);
-            move(card);
+        } else {
+            move(deck.get(deck.size() - 1));
         }
     }
+
     public void move(Card card) {
         waste.add(card);
         deck.remove(card);
-        waste.get(waste.size()-1).setVisible(true);
-        if(waste.size()>3)
-        {
+        waste.get(waste.size() - 1).setVisible(true);
+        if (waste.size() > 3) {
             deck.add(0, waste.get(0));
             deck.get(0).setVisible(false);
             waste.remove(0);
         }
     }
-   
+    
+    public boolean isMovedDeckToWaste(Card card) {
+        return true;
+    }
+
+    public Board getBoard() {
+        return board;
+    }
+
+    public void setBoard(Board board) {
+        this.board = board;
+    }
+
+    
 
 }
